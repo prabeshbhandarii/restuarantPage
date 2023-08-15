bf329138c657f596dfac931e4492f7950623a79e
sanopopular <prabeshbhandari842@gmail.com>
2023-08-15 21:47:04 +0545

    initial commit

--- script.js
+++ script.js
@@ -5,9 +5,29 @@ window.addEventListener("scroll", function(){
     bigWhiteText.classList.toggle("doNotDisplay",window.scrollY > 0);
 });
 
-function validateForm(){
-    let password = document.forms["myForm"]["password"].value;
-    if (password.length < 8) {
-        document.getElementById("psw").innerHTML = "Password must contain at least 8 characters.";
+const first_name = document.getElementById("first_name");
+const last_name = document.getElementById("last_name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const address = document.getElementById("address");
+const phone_no = document.getElementById("phone_no");
+const comment = document.getElementById("comment");
+const errorElement = document.getElementById("error-message");
+
+
+window.addEventListener("submit", (e) => {
+    let messages = [];
+    if(first_name.value === '' || first_name.value == null){
+        messages.push("This field cannot be left empty");
     }
-}
+    if(password.value.length <= 8){
+        messages.push("Password must be longer than 8 characters");
+    }
+    if(password.value.length >= 30){
+        messages.push("Password must be shorter than 30 characters");
+    }
+    if(messages.length > 0){
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
+    } 
+});
